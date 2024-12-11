ab445be9a0ff41a610390c57ea46a50e3c122df1
Victus <chyngyz.baybosunov@mail.ru>
2024-12-11 12:53:20 +0600

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -8,26 +8,23 @@ fi
 echo "Обновляем систему..."
 apt update && apt upgrade -y
 
-echo "Устанавливаем Docker, Docker Compose и другие зависимости..."
-apt install -y docker.io git
 
-apt install docker-compose git
+echo "Устанавливаем Docker, Docker Compose и другие зависимости..."
+apt install -y docker.io git docker-compose
+
 
 chmod +x /usr/local/bin/docker-compose
 
 docker --version
 docker-compose --version
 
-if [ ! -d "/home/ubuntu/django" ]; then
-  echo "Клонируем проект..."
-  git clone https://your-repository-url.git /home/ubuntu/django
-else
+if [ -d ".git" ]; then
   echo "Проект уже существует, выполняем git pull..."
-  cd /home/ubuntu/django
   git pull
+else
+  echo "Клонируем проект..."
+  git clone https://github.com/ChynChyngyz/Dep.git .
 fi
-
-cd /home/ubuntu/django
 
 echo "Запускаем Docker Compose..."
 docker-compose up --build -d
