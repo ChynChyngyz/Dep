2eb542b49594bef45c0a723b0b96716fdd9e0508
Victus <chyngyz.baybosunov@mail.ru>
2024-12-11 12:40:37 +0600

    Деплой1

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,8 @@ apt update && apt upgrade -y
 echo "Устанавливаем Docker, Docker Compose и другие зависимости..."
 apt install -y docker.io git
 
-curl -L https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+apt install docker-compose git
+
 chmod +x /usr/local/bin/docker-compose
 
 docker --version
