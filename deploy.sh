f25a237c3e77c7699dab339ee0b7039b8acc7cbc
Victus <chyngyz.baybosunov@mail.ru>
2024-12-12 23:05:34 +0600

    1

--- deploy.sh
+++ deploy.sh
@@ -8,22 +8,24 @@ fi
 echo "Обновляем систему..."
 apt update && apt upgrade -y
 
-
 echo "Устанавливаем Docker, Docker Compose и другие зависимости..."
 apt install -y docker.io git docker-compose
-
 
 chmod +x /usr/local/bin/docker-compose
 
 docker --version
 docker-compose --version
 
-if [ -d ".git" ]; then
+cd /var/www
+
+if [ -d "Dep" ]; then
   echo "Проект уже существует, выполняем git pull..."
+  cd Dep
   git pull
 else
   echo "Клонируем проект..."
   git clone https://github.com/ChynChyngyz/Dep.git .
+  cd Dep
 fi
 
 echo "Запускаем Docker Compose..."
--- service/views.py
+++ service/views.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
+from drf_spectacular.utils import extend_schema
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
